93973489753e9270a5cc1f46d7c325e33cd9f2e2
Jason Alvarado <alvaradojasondev@gmail.com>
2023-12-17 23:03:23 -0300

    trycatch general

--- UI/MainForm.cs
+++ UI/MainForm.cs
@@ -52,245 +52,342 @@ namespace UI
 
         private void btnLibros_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnLibros.BackColor = Color.LightBlue;
-            bookControl = new BookControl(bookService, authorService, categoryService, loanService, subscriptionService, subscriptionTypeService, notificationService, userService, user);
-            mainPanel.Controls.Add(bookControl);
-            bookControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnLibros.BackColor = Color.LightBlue;
+                bookControl = new BookControl(bookService, authorService, categoryService, loanService, subscriptionService, subscriptionTypeService, notificationService, userService, user);
+                mainPanel.Controls.Add(bookControl);
+                bookControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnNotificaciones_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnNotificaciones.BackColor = Color.LightBlue;
-            notifsControl = new NotifsControl(notificationService, user);
-            mainPanel.Controls.Add(notifsControl);
-            notifsControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnNotificaciones.BackColor = Color.LightBlue;
+                notifsControl = new NotifsControl(notificationService, user);
+                mainPanel.Controls.Add(notifsControl);
+                notifsControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnUsuarios_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnUsuarios.BackColor = Color.LightBlue;
-            usersControl = new UsuariosControl(userService, roleService, user);
-            mainPanel.Controls.Add(usersControl);
-            usersControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnUsuarios.BackColor = Color.LightBlue;
+                usersControl = new UsuariosControl(userService, roleService, user);
+                mainPanel.Controls.Add(usersControl);
+                usersControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnAuthors_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnAuthors.BackColor = Color.LightBlue;
-            authorControl = new AuthorControl(authorService, user);
-            mainPanel.Controls.Add(authorControl);
-            authorControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnAuthors.BackColor = Color.LightBlue;
+                authorControl = new AuthorControl(authorService, user);
+                mainPanel.Controls.Add(authorControl);
+                authorControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnCategories_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnCategories.BackColor = Color.LightBlue;
-            categoryControl = new CategoryControl(categoryService, user);
-            mainPanel.Controls.Add(categoryControl);
-            categoryControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnCategories.BackColor = Color.LightBlue;
+                categoryControl = new CategoryControl(categoryService, user);
+                mainPanel.Controls.Add(categoryControl);
+                categoryControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnBackup_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnBackup.BackColor = Color.LightBlue;
-            backupControl = new BackupControl(backupService, user);
-            mainPanel.Controls.Add(backupControl);
-            backupControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnBackup.BackColor = Color.LightBlue;
+                backupControl = new BackupControl(backupService, user);
+                mainPanel.Controls.Add(backupControl);
+                backupControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            DialogResult result = MessageBox.Show("Salir de la aplicacion?", "Confirmar", MessageBoxButtons.YesNo);
-            if (result == DialogResult.Yes)
+            try
             {
-                try
+                DialogResult result = MessageBox.Show("Salir de la aplicacion?", "Confirmar", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
                 {
-                    Application.Exit();
+                    try
+                    {
+                        Application.Exit();
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("App finalizada");
+                    }
                 }
-                catch (Exception)
-                {
-                    MessageBox.Show("App finalizada");
-                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
             }
         }
 
         private void btnSubsInfo_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnSubscriptionTypes.BackColor = Color.LightBlue;
-            subscriptionTypesControl = new SubscriptionTypeControl(subscriptionTypeService, subscriptionService, user);
-            mainPanel.Controls.Add(subscriptionTypesControl);
-            subscriptionTypesControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnSubscriptionTypes.BackColor = Color.LightBlue;
+                subscriptionTypesControl = new SubscriptionTypeControl(subscriptionTypeService, subscriptionService, user);
+                mainPanel.Controls.Add(subscriptionTypesControl);
+                subscriptionTypesControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnRoles_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnRoles.BackColor = Color.LightBlue;
-            rolsControl = new RolsControl(roleService);
-            mainPanel.Controls.Add(rolsControl);
-            rolsControl.BringToFront();
+            try
+            {
+                ResetButtonsColors();
+                btnRoles.BackColor = Color.LightBlue;
+                rolsControl = new RolsControl(roleService);
+                mainPanel.Controls.Add(rolsControl);
+                rolsControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            string username = txtUsername.Text;
-            string password = txtPassword.Text;
-
-            user = userService.Login(username, password);
-
-            if (user == null)
+            try
             {
-                MessageBox.Show("Usuario y/o contraseña no validos");
+                string username = txtUsername.Text;
+                string password = txtPassword.Text;
+
+                user = userService.Login(username, password);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Usuario y/o contraseña no validos");
+                }
+                else
+                {
+                    txtUsername.Enabled = false;
+                    txtPassword.Enabled = false;
+                    txtPassword.ResetText();
+
+                    btnLogin.Enabled = false;
+                    btnRegister.Enabled = false;
+
+                    HabilitarBotonesPorRol();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                txtUsername.Enabled = false;
-                txtPassword.Enabled = false;
-                txtPassword.ResetText();
-
-                btnLogin.Enabled = false;
-                btnRegister.Enabled = false;
-
-                HabilitarBotonesPorRol();
+                MessageBox.Show("Error ejecutando la orden");
             }
         }
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            string username = txtUsername.Text;
-            string password = txtUsername.Text;
-
             try
             {
+                string username = txtUsername.Text;
+                string password = txtUsername.Text;
+
                 userService.Create(new User(username, password, "Cliente"));
                 MessageBox.Show("Registrado Correctamente");
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Error ejecutando la orden");
             }
         }
 
         private void btnLogout_Click(object sender, EventArgs e)
         {
-            user = null;
-            HabilitarBotonesPorRol();
-
-            foreach (Control control in mainPanel.Controls.OfType<UserControl>())
+            try
             {
-                control.Visible = false;
+                user = null;
+                HabilitarBotonesPorRol();
+
+                foreach (Control control in mainPanel.Controls.OfType<UserControl>())
+                {
+                    control.Visible = false;
+                }
+
+                txtUsername.Enabled = true;
+                txtPassword.Enabled = true;
+                txtUsername.ResetText();
+
+                btnLogin.Enabled = true;
+                btnRegister.Enabled = true;
             }
-
-            txtUsername.Enabled = true;
-            txtPassword.Enabled = true;
-            txtUsername.ResetText();
-
-            btnLogin.Enabled = true;
-            btnRegister.Enabled = true;
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void btnEstadisticas_Click(object sender, EventArgs e)
         {
-            ResetButtonsColors();
-            btnEstadisticas.BackColor = Color.LightBlue;
+            try
+            {
+                ResetButtonsColors();
+                btnEstadisticas.BackColor = Color.LightBlue;
 
-            statsControl = new StatsControl();
-            mainPanel.Controls.Add(statsControl);
-            statsControl.BringToFront();
+                statsControl = new StatsControl();
+                mainPanel.Controls.Add(statsControl);
+                statsControl.BringToFront();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void ResetButtonsColors()
         {
-            btnLibros.BackColor = Color.LightGray;
-            btnNotificaciones.BackColor = Color.LightGray;
-            btnUsuarios.BackColor = Color.LightGray;
-            btnSubscriptionTypes.BackColor = Color.LightGray;
-            btnBackup.BackColor = Color.LightGray;
-            btnAuthors.BackColor = Color.LightGray;
-            btnCategories.BackColor = Color.LightGray;
-            btnRoles.BackColor = Color.LightGray;
-            btnEstadisticas.BackColor = Color.LightGray;
+            try
+            {
+                btnLibros.BackColor = Color.LightGray;
+                btnNotificaciones.BackColor = Color.LightGray;
+                btnUsuarios.BackColor = Color.LightGray;
+                btnSubscriptionTypes.BackColor = Color.LightGray;
+                btnBackup.BackColor = Color.LightGray;
+                btnAuthors.BackColor = Color.LightGray;
+                btnCategories.BackColor = Color.LightGray;
+                btnRoles.BackColor = Color.LightGray;
+                btnEstadisticas.BackColor = Color.LightGray;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ejecutando la orden");
+            }
         }
 
         private void HabilitarBotonesPorRol()
         {
-            btnNotificaciones.Visible = false;
-            btnLibros.Visible = false;
-            btnNotificaciones.Visible = false;
-            btnUsuarios.Visible = false;
-            btnRoles.Visible = false;
-            btnAuthors.Visible = false;
-            btnCategories.Visible = false;
-            btnSubscriptionTypes.Visible = false;
-            btnBackup.Visible = false;
-            btnEstadisticas.Visible = false;
-            btnLogout.Visible = false;
-            btnEstadisticas.Visible = false;
-
-            if (user == null)
+            try
             {
-                return;
+                btnNotificaciones.Visible = false;
+                btnLibros.Visible = false;
+                btnNotificaciones.Visible = false;
+                btnUsuarios.Visible = false;
+                btnRoles.Visible = false;
+                btnAuthors.Visible = false;
+                btnCategories.Visible = false;
+                btnSubscriptionTypes.Visible = false;
+                btnBackup.Visible = false;
+                btnEstadisticas.Visible = false;
+                btnLogout.Visible = false;
+                btnEstadisticas.Visible = false;
+                btnLogout.Visible = true;
+
+                if (user == null)
+                {
+                    return;
+                }
+
+                List<string> permisosGenerales = user?.Permisos?.Select(x => x.Split('.')?[0])?.Distinct()?.ToList() ?? new List<string>();
+
+                if (permisosGenerales.Contains("Libro"))
+                {
+                    btnLibros.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("TipoSubscripcion"))
+                {
+                    btnSubscriptionTypes.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Usuario"))
+                {
+                    btnUsuarios.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Autor"))
+                {
+                    btnAuthors.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Backup"))
+                {
+                    btnBackup.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Categoria"))
+                {
+                    btnCategories.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Notificacion"))
+                {
+                    btnNotificaciones.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Subscripcion"))
+                {
+                    btnSubscriptionTypes.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Roles"))
+                {
+                    btnRoles.Visible = true;
+                }
+
+                if (permisosGenerales.Contains("Estadisticas"))
+                {
+                    btnEstadisticas.Visible = true;
+                }
             }
-
-            btnLogout.Visible = true;
-
-            List<string> permisosGenerales = user?.Permisos?.Select(x => x.Split('.')?[0])?.Distinct()?.ToList() ?? new List<string>();
-
-            if (permisosGenerales.Contains("Libro"))
+            catch (Exception ex)
             {
-                btnLibros.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("TipoSubscripcion"))
-            {
-                btnSubscriptionTypes.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Usuario"))
-            {
-                btnUsuarios.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Autor"))
-            {
-                btnAuthors.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Backup"))
-            {
-                btnBackup.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Categoria"))
-            {
-                btnCategories.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Notificacion"))
-            {
-                btnNotificaciones.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Subscripcion"))
-            {
-                btnSubscriptionTypes.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Roles"))
-            {
-                btnRoles.Visible = true;
-            }
-
-            if (permisosGenerales.Contains("Estadisticas"))
-            {
-                btnEstadisticas.Visible = true;
+                MessageBox.Show("Error ejecutando la orden");
             }
         }
 
